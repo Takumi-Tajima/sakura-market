a838128c12235f0d30a81cd1052e28190d9de1ee
taji <taji@sonicgarden.jp>
2024-09-04 18:13:28 +0900

    adminのカラムにバリデーションを追加する

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,5 @@
 class Admin < ApplicationRecord
   devise :database_authenticatable, :rememberable, :validatable
+  validates :email, :password, presence: true
+  validates :password, length: { minimum: 8 }
 end
