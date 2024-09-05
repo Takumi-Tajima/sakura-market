82ab55c35eb2bf0c1672b3c7ab5cda538de37d3c
taji <taji@sonicgarden.jp>
2024-09-05 11:11:48 +0900

    不要なバリデーションを削除する

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,3 @@
 class Admin < ApplicationRecord
   devise :database_authenticatable, :rememberable, :validatable
-  validates :email, :password, presence: true
-  validates :password, length: { minimum: 8 }
 end
