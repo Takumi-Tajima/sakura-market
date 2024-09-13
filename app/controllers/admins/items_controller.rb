df04aefc46fc456cd6b09151a8e150b71c386f2a
taji <taji@sonicgarden.jp>
2024-09-13 16:01:10 +0900

    hiddenからpublishedに変更

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -45,6 +45,6 @@ class Admins::ItemsController < Admins::ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:name, :price, :description, :hidden)
+    params.require(:item).permit(:name, :price, :description, :published)
   end
 end
