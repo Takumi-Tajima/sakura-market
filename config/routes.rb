5d37ef701ac93a9855175934e51228ff1ed75f2d
taji <taji@sonicgarden.jp>
2024-09-14 15:46:22 +0900

    destroyのタイポを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'admins/items#index'
   namespace :admins do
-    resources :items, only: %i[index new create edit update destory]
+    resources :items, only: %i[index new create edit update destroy]
   end
   devise_for :admins
 end
