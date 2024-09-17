4e3ebf14d54b1ac86b6a094bc0d1b760efe4cad5
taji <taji@sonicgarden.jp>
2024-09-17 16:08:02 +0900

    ユーザーのitemsテストを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  namespace :users do
+    resources :items
+  end
   namespace :admins do
     resources :items, only: %i[index new create edit update destroy]
   end
