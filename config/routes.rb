ec18d10431820b427cf4693de44689fd830ed21c
taji <taji@sonicgarden.jp>
2024-09-17 20:11:16 +0900

    管理者とユーザーのレイアウトを分ける

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  namespace :users do
+  scope module: :users do
     resources :items
   end
   namespace :admins do
+    root 'items#index'
     resources :items, only: %i[index new create edit update destroy]
   end
   devise_for :admins
