fd79c0000ef28b85c6036d35b3b1840e770f9582
taji <taji@sonicgarden.jp>
2024-09-18 21:19:50 +0900

    税込価格表示テスト修正

--- spec/system/items_spec.rb
+++ spec/system/items_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe '商品管理機能', type: :system do
   context 'ログインしてない時' do
-    let!(:item) { create(:item, name: '大根', price: '¥200') }
+    let!(:item) { create(:item, name: '大根', price: '200') }
 
     it '商品の一覧ページを閲覧できること' do
       visit root_path
       expect(page).to have_content '大根'
-      expect(page).to have_content '¥200'
+      expect(page).to have_content '¥220'
     end
   end
 end
