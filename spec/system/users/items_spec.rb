51532d7f52ac0c9dba2bd208e2360ab61ba5613d
taji <taji@sonicgarden.jp>
2024-09-18 15:26:46 +0900

    一覧ページはログインしてなくても閲覧できる

--- spec/system/users/items_spec.rb
+++ spec/system/users/items_spec.rb
@@ -1,15 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe '商品管理機能', type: :system do
-  context 'ログインしている時' do
-    let(:user) { create(:user) }
+  context 'ログインしてない時' do
     let!(:item) { create(:item, name: '大根', price: '200') }
 
-    before do
-      sign_in user
-    end
-
-    xit '商品の一覧ページを閲覧できること' do
+    it '商品の一覧ページを閲覧できること' do
       visit root_path
       expect(page).to have_content '大根'
       expect(page).to have_content '200'
