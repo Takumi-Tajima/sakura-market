b4f1a7734925d9e6c6ad4dd9312f4c5eec455721
taji <taji@sonicgarden.jp>
2024-09-19 11:01:57 +0900

    詳細画面を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'items#index'
   devise_for :users
-  resources :items, only: %i[index]
+  resources :items, only: %i[index show]
   namespace :admins do
     root 'items#index'
     resources :items, only: %i[index new create edit update destroy]
