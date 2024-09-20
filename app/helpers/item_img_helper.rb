d8beda81fac41a15a49cc2801dc7d8b96ceb5474
taji <taji@sonicgarden.jp>
2024-09-20 15:06:45 +0900

    item_img_helper編集

--- app/helpers/item_img_helper.rb
+++ app/helpers/item_img_helper.rb
@@ -1,5 +1,7 @@
 module ItemImgHelper
   def item_img_tag(item, size = :thumb)
-    image_tag item.item_img.variant(size)
+    if item.item_img.attached?
+      image_tag item.item_img.variant(size)
+    end
   end
 end
