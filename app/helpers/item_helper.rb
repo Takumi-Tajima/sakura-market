9cbcc7c55fd7f1a8d80797e2371618445dd748bd
taji <taji@sonicgarden.jp>
2024-09-20 14:01:57 +0900

    税込価格表示helperの名前を変更する

--- app/helpers/item_helper.rb
+++ app/helpers/item_helper.rb
@@ -1,5 +1,5 @@
 module ItemHelper
-  def p_with_tax(item)
+  def price_with_tax(item)
     t('items.price_with_tax', price: number_to_currency(item.price_with_tax, unit: '¥', precision: 0))
   end
 end
