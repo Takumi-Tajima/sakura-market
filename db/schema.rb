09a39e07d28c929a8447b3d67e6fe72ca9192efd
taji <taji@sonicgarden.jp>
2024-09-25 10:04:01 +0900

    positionカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_19_083047) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_25_003707) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_19_083047) do
     t.boolean "published", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
   end
 
   create_table "users", force: :cascade do |t|
