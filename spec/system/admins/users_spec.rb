57e48d2af70d5ffef6312d503ea06388c65cf76f
taji <taji@sonicgarden.jp>
2024-09-30 15:53:50 +0900

    users_specの修正

--- spec/system/admins/users_spec.rb
+++ spec/system/admins/users_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe '管理者のユーザー管理', type: :system do
   let(:admin) { create(:admin) }
-  let!(:user1) { create(:user, email: 'test@example.com') }
-  let!(:user2) { create(:user, email: 'fuga@example.com') }
+  let!(:user1) { create(:user, email: 'test@example.com', password: 'password') }
+  let!(:user2) { create(:user, email: 'fuga@example.com', password: 'password') }
 
   before do
     sign_in admin
@@ -18,21 +18,22 @@ RSpec.describe '管理者のユーザー管理', type: :system do
   it 'ユーザーの情報を更新できる' do
     visit edit_admins_user_path(user1)
     fill_in 'メール',	with: 'takumi@example.com'
+    fill_in 'パスワード',	with: 'password'
     click_on '登録'
     expect(page).to have_content '更新に成功しました'
     expect(page).to have_content 'takumi@example.com'
-    expect(page).to have_content 'test@example.com'
+    expect(page).not_to have_content 'test@example.com'
   end
 
   it 'ユーザーを削除できる' do
     visit admins_users_path
     item_row = page.find('table tr', text: 'test@example.com')
     within(item_row) do
-      accept_confirm('本当に削除しますか？') do
+      accept_confirm('本当に削除しますか') do
         click_on '削除'
       end
     end
     expect(page).to have_content 'ユーザーを削除しました'
-    expect(page).to have_content 'test@example.com'
+    expect(page).not_to have_content 'test@example.com'
   end
 end
