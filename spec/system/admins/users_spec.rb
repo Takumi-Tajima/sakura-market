b1543253dbac00f6ad7ee2b817829d746fd41368
taji <taji@sonicgarden.jp>
2024-10-01 17:52:58 +0900

    ユーザー一覧参照のテスト修正

--- spec/system/admins/users_spec.rb
+++ spec/system/admins/users_spec.rb
@@ -11,7 +11,8 @@ RSpec.describe '管理者のユーザー管理', type: :system do
 
   it 'ユーザーの一覧を参照できる' do
     visit admins_users_path
-    expect(page).to have_content 'test@example.com'
-    expect(page).to have_content 'fuga@example.com'
+    trows = all('tr').map(&:text)
+    expect(trows[1]).to have_content 'test@example.com'
+    expect(trows[2]).to have_content 'fuga@example.com'
   end
 end
