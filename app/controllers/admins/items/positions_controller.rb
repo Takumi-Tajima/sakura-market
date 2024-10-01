4652d90666aebcdbb74d506acc32e8d315130425
taji <taji@sonicgarden.jp>
2024-10-01 15:58:27 +0900

    入れ替えてテスト修正

--- app/controllers/admins/items/positions_controller.rb
+++ app/controllers/admins/items/positions_controller.rb
@@ -2,12 +2,12 @@ class Admins::Items::PositionsController < ApplicationController
   def move_higher
     item = Item.find(params[:id])
     item.move_higher
-    redirect_to admins_item_path(item)
+    redirect_to admins_items_path
   end
 
   def move_lower
     item = Item.find(params[:id])
     item.move_lower
-    redirect_to admins_item_path(item)
+    redirect_to admins_items_path
   end
 end
