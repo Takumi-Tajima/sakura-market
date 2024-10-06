32b5b63fc8ad448fd82564fe617b3894c0d9e98a
taji <taji@sonicgarden.jp>
2024-10-06 22:57:23 +0900

    destroyアクションを修正

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -15,8 +15,8 @@ class CartItemsController < ApplicationController
   end
 
   def destroy
-    cart_item = CartItem.find(params[:id])
-    cart_item.destroy
+    cart_item = current_user.cart_items.find(params[:id])
+    cart_item.destroy!
     redirect_to cart_items_path, notice: 'カートから商品が削除されました。'
   end
 
